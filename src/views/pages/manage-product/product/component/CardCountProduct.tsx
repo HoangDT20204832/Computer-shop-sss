32d9abb6c323c5a9d17b86a3a2495edaebdb0d93
HoangDT20204832 <duongtienhoang2002@gmail.com>
2024-12-22 12:20:58 +0700

    thống kê số lượng sản phẩm theo public, private

--- src/views/pages/manage-product/product/component/CardCountProduct.tsx
+++ src/views/pages/manage-product/product/component/CardCountProduct.tsx
@@ -1,3 +1,4 @@
+
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
@@ -12,19 +13,19 @@ import { useTranslation } from 'react-i18next'
 
 // ** Custom Component Import
 
-export type CardCountUserProps = {
+export type CardCountProductProps = {
     icon: string
     sx?: SxProps<Theme>
     avatarSize?: number
     iconSize?: number | string,
     userType: number,
-    countUserType: {
+    countProductStatus: {
         data: Record<number, number>,
-        totalUser: number
+        total: number
     }
 }
 
-const CardCountUser = (props: any) => {
+const CardCountProduct = (props: any) => {
 
     // ** Hooks
     const theme = useTheme()
@@ -34,53 +35,48 @@ const CardCountUser = (props: any) => {
        const {
         sx,
         icon,
-        countUserType,
+        countProductStatus,
         count,
         iconSize = 24,
         avatarSize = 38,
-        userType
+        status
     } = props
 
-    const mapUserType = {
+    const mapProductStatus = {
         1: {
-            title: t("Facebook Users"),
-            count: countUserType?.data?.[1] | 0,
+            title: t("Public Products"),
+            count: countProductStatus?.data?.[1],
             themeColor: theme.palette.success.main
         },
         2: {
-            title: t("Google Users"),
-            count: countUserType?.data?.[2] | 0,
-            themeColor: theme.palette.error.main
-        },
-        3: {
-            title: t("Email Users"),
-            count: countUserType?.data?.[3] | 0,
+            title: t("Total Product"),
+            count: countProductStatus?.total,
             themeColor: theme.palette.info.main
         },
-        4: {
-            title: t("Total Users"),
-            count: countUserType?.totalUser | 0,
-            themeColor: theme.palette.primary.main
-        }
+        0: {
+            title: t("Private Products"),
+            count: countProductStatus?.data?.[0],
+            themeColor: theme.palette.error.main
+        },
     }
-console.log("hsg",countUserType)
+
  
 
     return (
-        <Card sx={{ ...sx,backgroundColor: hexToRGBA((mapUserType as any)[userType]?.themeColor, 0.8) }}>
+        <Card sx={{ ...sx,backgroundColor: hexToRGBA((mapProductStatus as any)[status]?.themeColor, 0.7) }}>
             <CardContent sx={{ gap: 3, display: 'flex', justifyContent: 'space-between' }}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                    <Typography sx={{ mb: 1, color: theme.palette.customColors.lightPaperBg }}>{(mapUserType as any)[userType]?.title}</Typography>
+                    <Typography sx={{ mb: 1, color: theme.palette.customColors.lightPaperBg }}>{(mapProductStatus as any)[status]?.title}</Typography>
                     <Box sx={{ mb: 1, columnGap: 1.5, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-                        <Typography variant='h4' sx={{ color: theme.palette.customColors.lightPaperBg, fontWeight: "bold" }}>{(mapUserType as any)[userType]?.count}</Typography>
+                        <Typography variant='h4' sx={{ color: theme.palette.customColors.lightPaperBg, fontWeight: "bold" }}>{(mapProductStatus as any)[status]?.count}</Typography>
                     </Box>
                 </Box>
                 <Avatar variant='rounded' sx={{ width: avatarSize, height: avatarSize }}>
-                    <Icon icon={icon} fontSize={iconSize} color={(mapUserType as any)[userType]?.themeColor} />
+                    <Icon icon={icon} fontSize={iconSize} color={(mapProductStatus as any)[status]?.themeColor} />
                 </Avatar>
             </CardContent>
         </Card>
     )
 }
 
-export default CardCountUser
+export default CardCountProduct
